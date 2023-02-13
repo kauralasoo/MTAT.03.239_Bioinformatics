0cc4277eb68b70a10ea7352db035d803cae9da25
Kaur Alasoo <kauralasoo@gmail.com>
2023-02-13 10:32:38 +0200

    Create build_featureCounts_container.sh

---
+++ Nextflow_example/build_featureCounts_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --time=2:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=4G
+#SBATCH --job-name="nf-featureCounts"
+#SBATCH --partition=testing
+
+# Load needed system tools (Java 8 is required, one of singularity or anaconda - python 2.7 is needed,
+# depending on the method for dependancy management). The exact names of tool modules might depend on HPC.
+module load any/singularity/3.7.3
+module load squashfs/4.4
+
+singularity build featureCounts.img docker://quay.io/eqtlcatalogue/rnaseq:v20.11.1
